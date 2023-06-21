73df629630b4c1e4944b4417fd785a3ea20855fd
olhkyle <kylekwon96@gmail.com>
2023-06-21 20:05:14 +0900

    [FEAT] add DOMString in Card and CardList component

--- components/Card.js
+++ components/Card.js
@@ -2,8 +2,14 @@ import Component from '../library/Component.js';
 
 class Card extends Component {
 	render() {
+		const { card } = this.props;
+
 		return `
-		
+				<div>
+					<p>${card.title}</p>
+					<i class="bx bx-pencil card-menu"></i>
+				</div>
+				${card.description ? `<i class="bx bx-menu-alt-left has-desc"></i>` : ''}
 		`;
 	}
 }
--- components/CardList.js
+++ components/CardList.js
@@ -1,11 +1,16 @@
 import Component from '../library/Component.js';
+import Card from './Card.js';
 
 class CardList extends Component {
 	render() {
-		// const { lists, listcomposer } = this.props;
+		const { cards } = this.props;
 
+		// prettier-ignore
 		return `
-    
+				${cards.map(card => `
+					<div data-card-id="${card.id}" class="card" draggable="true">
+						${new Card({ card }).render()}
+					</div>`).join('')}
     `;
 	}
 }
