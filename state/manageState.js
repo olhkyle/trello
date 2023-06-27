cefa29e4fa7e497d49437ef9c193ff2ee56ffefc
olhkyle <kylekwon96@gmail.com>
2023-06-27 19:08:16 +0900

    [FEAT] implement generateCardNextId and update function names

--- state/manageState.js
+++ state/manageState.js
@@ -1,8 +1,10 @@
-const generateListNextId = lists => Math.max(...lists.map(({ id }) => id), 0) + 1;
+const generateNextListId = lists => Math.max(...lists.map(({ id }) => id), 0) + 1;
+
+const generateNextCardId = cards => Math.max(...cards.map(({ id }) => id), 0) + 1;
 
 const toggleIsCardCreatorOpen = (lists, listId) =>
 	lists.map(list => (list.id === listId ? { ...list, isCardCreatorOpen: !list.isCardCreatorOpen } : list));
 
 const removeListByClickedId = (lists, listId) => lists.filter(({ id }) => id !== listId);
 
-export { generateListNextId, toggleIsCardCreatorOpen, removeListByClickedId };
+export { generateListNextId, generateCardNextId, toggleIsCardCreatorOpen, removeListByClickedId };
